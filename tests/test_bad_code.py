68068794538f5743d5ca350d5426038b65c7cb8c
DavidAltman920 <david01altman@gmail.com>
2026-05-13 16:22:05 -0400

    changing sth functionally

--- tests/test_bad_code.py
+++ tests/test_bad_code.py
@@ -3,4 +3,4 @@ from bad_code import poorly_formatted
 
 def test_poorly_formatted():
     # This test will fail intentionally
-    assert poorly_formatted(5, 3) == 3  # Wrong! Should be 5
+    assert poorly_formatted(5, 3) == 5  # Wrong! Should be 5
